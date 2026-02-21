6def1a74bb3960145be97fe8d5a154e441e5cbdd
Avi Gupta <agupta07@stanford.edu>
2026-02-21 18:07:51 -0500

    Add Chromium and Playwright dependencies in Modal

--- web_scraper.py
+++ web_scraper.py
@@ -5,14 +5,42 @@ import modal
 
 app = modal.App("example-scraper")
 
-@app.function()
-def get_links(url):
-    response = urllib.request.urlopen(url)
-    html = response.read().decode("utf8")
-    links = []
-    for match in re.finditer('href="(.*?)"', html):
-        links.append(match.group(1))
-    return links
+# Custom image to install Chromium and Playwright in Modal.
+playwright_image = modal.Image.debian_slim(python_version="3.10").run_commands(
+    "apt-get update",
+    "apt-get install -y software-properties-common",
+    "apt-add-repository non-free",
+    "apt-add-repository contrib",
+    "pip install playwright==1.42.0",
+    "playwright install-deps chromium",
+    "playwright install chromium",
+)
+
+@app.function(image=playwright_image)
+async def get_links(cur_url: str) -> list[str]:
+    from playwright.async_api import (
+        TimeoutError as PlaywrightTimeoutError,
+        async_playwright,
+    )
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+
+        try:
+            await page.goto(cur_url, timeout=10_000)  # ten seconds
+        except PlaywrightTimeoutError:
+            print(f"Timeout loading {cur_url}, skipping")
+            await browser.close()
+            return []
+
+        links = await page.eval_on_selector_all(
+            "a[href]", "elements => elements.map(element => element.href)"
+        )
+        await browser.close()
+
+    print("Links", links)
+    return list(set(links))
 
 @app.local_entrypoint()
 def main(url):
